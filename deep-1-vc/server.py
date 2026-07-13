c97413ac599c7d185fb6e29ed4d31929c53c2d05
fendoz <efendi.hariyadi@gmail.com>
2026-07-13 21:00:09 +0800

    add fav icon to all html

--- deep-1-vc/server.py
+++ deep-1-vc/server.py
@@ -5,9 +5,11 @@ import argparse
 
 DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
+
 class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- server.py
+++ server.py
@@ -6,26 +6,28 @@ import webbrowser
 # Mengatur argumen baris perintah (CLI)
 parser = argparse.ArgumentParser(description="Jalankan static file server.")
 parser.add_argument(
-    "--port", 
-    type=int, 
-    default=8000, 
-    help="Nomor port untuk server lokal (default: 8000)"
+    "--port",
+    type=int,
+    default=8000,
+    help="Nomor port untuk server lokal (default: 8000)",
 )
 args = parser.parse_args()
 
 PORT = args.port
 Handler = http.server.SimpleHTTPRequestHandler
 
-print(f"Melayani file statis di http://localhost:{PORT}")
-print("Tekan Ctrl+C untuk menghentikan server.")
 
-# Membuka browser secara otomatis
-webbrowser.open(f"http://localhost:{PORT}")
+if __name__ == "__main__":
+    print(f"Melayani file statis di http://localhost:{PORT}")
+    print("Tekan Ctrl+C untuk menghentikan server.")
 
-# Menjalankan server
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        print("\nServer dihentikan.")
-        httpd.server_close()
+    # Membuka browser secara otomatis
+    webbrowser.open(f"http://localhost:{PORT}")
+
+    # Menjalankan server
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\nServer dihentikan.")
+            httpd.server_close()
